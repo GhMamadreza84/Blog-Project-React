47471c5e876209060bee488cf26d886cac22183e
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-06-14 09:51:24 -0700

    wrap apolloprovider in index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ApolloClient,ApolloProvider,InMemoryCache } from "@apollo/client";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
 import App from "./App";
 
 import "./styles/index.css";
 import "./styles/fonts.css";
 
+const client = new ApolloClient({
+  uri: "",
+  cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  
-        <App />
-      
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
 );
