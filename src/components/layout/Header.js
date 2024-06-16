c1912794a2a8caa554f3657b59e537b0ccf4917d
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-06-16 09:21:58 -0700

    add typography

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,12 +1,14 @@
-import React from 'react'
-import { AppBar } from '@mui/material'
+import React from "react";
+import { AppBar, Toolbar, Typography } from "@mui/material";
 
 function Header() {
   return (
     <AppBar position="sticky">
-
+      <Toolbar>
+        <Typography component="h1" variant="h5"></Typography>
+      </Toolbar>
     </AppBar>
-  )
+  );
 }
 
-export default Header
+export default Header;
