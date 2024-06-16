8c86f900debcd5c35d823b066bce515f9c3ecc03
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-06-16 09:27:07 -0700

    add flex = 1 to typography

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -5,7 +5,7 @@ function Header() {
   return (
     <AppBar position="sticky">
       <Toolbar>
-        <Typography component="h1" variant="h5">وبلاگ محمدرضا قدیری</Typography>
+        <Typography component="h1" variant="h5" flex={1}>وبلاگ محمدرضا قدیری</Typography>
         <BookIcon />
       </Toolbar>
     </AppBar>
