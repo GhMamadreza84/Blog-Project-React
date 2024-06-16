6357b41a4d50198dcc86b6ade7c9ff0a4a7fc406
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-06-16 09:25:26 -0700

    material icon installed and use bookicon in header

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { AppBar, Toolbar, Typography } from "@mui/material";
-
+import BookIcon from '@mui/icons-material/Book';
 function Header() {
   return (
     <AppBar position="sticky">
       <Toolbar>
         <Typography component="h1" variant="h5">وبلاگ محمدرضا قدیری</Typography>
-        
+        <BookIcon />
       </Toolbar>
     </AppBar>
   );
