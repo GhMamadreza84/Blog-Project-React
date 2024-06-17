4ea2b6018ad253f01497e80eb4f3ca0cd5f80d3f
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-06-17 09:08:24 -0700

    edit homepage

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -6,7 +6,7 @@ import Blogs from "../blog/Blogs";
 function HomePage() {
   return (
     <Container maxWidth="lg">
-      <Grid container>
+      <Grid container spacing={2}>
         <Grid item xs={12} md={3} mt={4}>
           <Typography component="h3" variant="h5" mb={3} fontWeight={700}>نویسنده ها</Typography>
           <Authors />
