2782612cea872a01b04c8ca4e1b6841d7c18246e
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-06-17 09:30:37 -0700

    edit app component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import { gql, useQuery } from "@apollo/client";
 import Header from "./components/layout/Header";
 import HomePage from "./components/home/HomePage";
+import Layout from "./components/layout";
 
-const QUERY = gql`
-  query {
-    authors {
-      name
-    }
-  }
-`;
 
 function App() {
-  const {loading, data} = useQuery(QUERY);
-  console.log(data);
+
   return (
     <>
-    <Header />
-    <HomePage />    
+      <Layout>
+        <HomePage />
+      </Layout>
     </>
   );
 }
