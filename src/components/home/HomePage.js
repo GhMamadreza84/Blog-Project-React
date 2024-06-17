62fd13f464c4ea7f84bee1e6db10826fb08c9eec
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-06-17 08:54:48 -0700

    import container in homepage and use it

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Container } from '@mui/material'
 
 function HomePage() {
   return (
-    <div>
-      
-    </div>
+    <Container>
+
+    </Container>
   )
 }
 
