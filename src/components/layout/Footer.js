587092ca61626eecdef5fbab774636ecd99a4378
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-06-17 09:25:14 -0700

    add typography to footer component

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,10 +1,13 @@
+import { Typography } from '@mui/material'
 import React from 'react'
 
 function Footer() {
   return (
-    <div>
-      
-    </div>
+    <footer>
+      <Typography>
+        
+      </Typography>
+    </footer>
   )
 }
 
