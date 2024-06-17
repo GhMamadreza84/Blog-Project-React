4928dbee31962b8065a6d1ff2f028312eb689242
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-06-17 09:30:49 -0700

    edit app component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import { gql, useQuery } from "@apollo/client";
-import Header from "./components/layout/Header";
 import HomePage from "./components/home/HomePage";
 import Layout from "./components/layout";
 
