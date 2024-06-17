d3a0e209028c8cb564ca190b084eb689184270db
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-06-17 03:06:21 -0700

    add font-weight in theme js for apply on mui

--- src/mui/theme.js
+++ src/mui/theme.js
@@ -1,6 +1,12 @@
 import { createTheme } from "@mui/material";
 const theme = createTheme({
-  typography:{
-    fontFamily:`"YekanBakh" , "Roboto" , "Arial"`
-  }
+  typography: {
+    fontFamily: `"YekanBakh" , "Roboto" , "Arial"`,
+    fontWeightLight: 300,
+    fontWeightRegular: 400,
+    fontWeightMedium: 500,
+    fontWeightBold: 700,
+    fontWeightHeavy: 800,
+    fontWeightFat: 900,
+  },
 });
