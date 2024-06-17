0233106520d9fdbee16a9f8b27fcdc6530a6b598
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-06-17 09:03:32 -0700

    import and use authors and blogs component

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { Container, Typography, Grid } from "@mui/material";
+import Authors from "../author/Authors";
+import Blogs from "../blog/Blogs";
 
 function HomePage() {
   return (
     <Container maxWidth="lg">
       <Grid container>
         <Grid item>
-          <Typography component="h3" variant="h5" mb={3}>نویسنده ها</Typography>
+          <Typography component="h3" variant="h5" mb={3} fontWeight={700}>نویسنده ها</Typography>
+          <Authors />
         </Grid>
         <Grid item>
-          <Typography>مقالات</Typography>
+          <Typography component="h3" variant="h5" mb={3} fontWeight={700}>مقالات</Typography>
+          <Blogs />
         </Grid>
       </Grid>
     </Container>
