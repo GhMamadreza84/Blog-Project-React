bf33db230b56f0dad2efdbd56e2f9c83817d008d
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-06-17 09:32:31 -0700

    edit footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <footer>
       <Typography component="p" variant='p' bgcolor="#f7f7f7" color="primary" padding="10px" textAlign="center" mt={10}>
-        پروژه وبلاگ با GraphQl - GhMamadreza
+      GhMamadreza  -  پروژه وبلاگ با GraphQl 
       </Typography>
     </footer>
   )
