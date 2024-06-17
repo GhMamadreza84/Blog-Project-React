e3fa8964d2214272cc44cd6d6ab3a557ec9e44e0
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-06-17 08:58:13 -0700

    add typography in grid in homepage component

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,12 +1,19 @@
 import React from 'react'
-import { Container } from '@mui/material'
+import { Container, Typography } from '@mui/material'
 import { Grid } from 'react-loader-spinner'
 
 function HomePage() {
   return (
     <Container maxWidth="lg">
-      <Grid>
-        
+      <Grid item>
+        <Typography>
+          نویسنده ها
+        </Typography>
+      </Grid>
+      <Grid item>
+        <Typography>
+          مقالات
+        </Typography>
       </Grid>
     </Container>
   )
