a84d7506f0cc4cf889f536b375e602f63bdec60b
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-06-17 09:27:26 -0700

    add another styles to footter component

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -4,8 +4,8 @@ import React from 'react'
 function Footer() {
   return (
     <footer>
-      <Typography component="p" variant='p' bgcolor="#f7f7f7" color="primary">
-
+      <Typography component="p" variant='p' bgcolor="#f7f7f7" color="primary" padding="10px" textAlign="center" mt={10}>
+        پروژه وبلاگ با GraphQl 
       </Typography>
     </footer>
   )
