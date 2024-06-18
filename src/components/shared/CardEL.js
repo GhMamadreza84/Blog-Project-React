baeada85fa362469d02598a98bb70d4f9777ba82
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-06-18 10:07:18 -0700

    add boxShadow to card in CardEl

--- src/components/shared/CardEL.js
+++ src/components/shared/CardEL.js
@@ -1,16 +1,22 @@
-import React from 'react'
-import {Card, CardActions, CardContent, CardHeader, CardMedia } from '@mui/material'
+import React from "react";
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  CardMedia,
+} from "@mui/material";
 
 function CardEL(props) {
-  console.log(props)
+  console.log(props);
   return (
-    <Card>
+    <Card sx={{ boxShadow: "rgba(0,0,0,0.1),0px 4px 12px" }}>
       <CardHeader />
       <CardMedia />
       <CardContent></CardContent>
       <CardActions></CardActions>
     </Card>
-  )
+  );
 }
 
-export default CardEL
+export default CardEL;
