4e028a41c14fffc384bbf6a9a8eb62eb8bd59ee2
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-06-18 09:12:08 -0700

    add query to queries file

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,5 +1,20 @@
 import { gql } from "@apollo/client";
 
 const GET_BLOGS_INFO = gql`
-
-`
+  query {
+    posts {
+      author {
+        name
+        avatar {
+          url
+        }
+      }
+      title
+      slug
+      id
+      coverPhoto {
+        url
+      }
+    }
+  }
+`;
