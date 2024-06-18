d6d5182588e63ebb35f1ac0b023bd7bc4547f22f
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-06-18 09:13:36 -0700

    import get_blogs_info in blogs and us it

--- src/components/blog/Blogs.js
+++ src/components/blog/Blogs.js
@@ -1,8 +1,9 @@
 import { useQuery } from '@apollo/client'
 import React from 'react'
+import GET_BLOGS_INFO from '../../graphql/queries'
 
 function Blogs() {
-  const { loading,data,errors}=useQuery()
+  const { loading,data,errors}=useQuery(GET_BLOGS_INFO)
   return (
     <div>
       Blogs
