f459e0e93fc0ff0fe8bff863deb03ed4a8bd709f
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-06-18 09:18:25 -0700

    add grid container in blogs

--- src/components/blog/Blogs.js
+++ src/components/blog/Blogs.js
@@ -1,6 +1,7 @@
 import { useQuery } from "@apollo/client";
 import React from "react";
 import { GET_BLOGS_INFO } from "../../graphql/queries";
+import { Grid } from "@mui/material";
 
 function Blogs() {
   const { loading, data, errors } = useQuery(GET_BLOGS_INFO);
@@ -8,7 +9,11 @@ function Blogs() {
   if (loading) return <h3>Loading ...</h3>;
   if (errors) return <h3>Error ...</h3>;
   console.log(data);
-  return <div>Blogs</div>;
+  return 
+  <Grid container spacing={2}>
+    
+  </Grid>
+  ;
 }
 
 export default Blogs;
