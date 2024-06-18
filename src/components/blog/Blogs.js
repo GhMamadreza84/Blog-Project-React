833e87a4785c6d314a3ccaea0da0769f4029cfcc
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-06-18 09:16:48 -0700

    add if to blogs for loading and when we have error

--- src/components/blog/Blogs.js
+++ src/components/blog/Blogs.js
@@ -1,10 +1,13 @@
 import { useQuery } from "@apollo/client";
 import React from "react";
-import {GET_BLOGS_INFO} from "../../graphql/queries";
+import { GET_BLOGS_INFO } from "../../graphql/queries";
 
 function Blogs() {
   const { loading, data, errors } = useQuery(GET_BLOGS_INFO);
-  console.log(data)
+
+  if (loading) return <h3>Loading ...</h3>;
+  if (errors) return <h3>Error ...</h3>;
+  console.log(data);
   return <div>Blogs</div>;
 }
 
