5095ec8fbef2f9eb00f017366dd6062e54631397
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-06-18 10:09:40 -0700

    edit boxShadow in CardEl component

--- src/components/shared/CardEL.js
+++ src/components/shared/CardEL.js
@@ -10,7 +10,7 @@ import {
 function CardEL(props) {
   console.log(props);
   return (
-    <Card sx={{ boxShadow: "rgba(0,0,0,0.1),0px 4px 12px",borderRadius:4, }}>
+    <Card sx={{ boxShadow: "rgba(0,0,0,0.1) 0px 4px 12px", borderRadius: 4 }}>
       <CardHeader />
       <CardMedia />
       <CardContent></CardContent>
