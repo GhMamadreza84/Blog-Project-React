5a55833fda16357c0d34f83fb1f2ffd05be53936
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-06-19 03:07:39 -0700

    add if for loading and show error when we have

--- src/components/author/Authors.js
+++ src/components/author/Authors.js
@@ -4,6 +4,8 @@ import { GET_AUTHORS_INFO } from '../../graphql/queries'
 
 function Authors() {
   const {loading , data ,errors}= useQuery(GET_AUTHORS_INFO)
+  if(loading) return <h3>Loading ...</h3>
+  if(errors) return <h3>Errors ...</h3>
   return (
     <div>
       Authors
