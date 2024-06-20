f194a84fe3e329dc37646b7ed4c2b605b94c011d
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-06-20 10:00:11 -0700

    add route in app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./components/home/HomePage";
 import Layout from "./components/layout";
+import BlogPage from "./components/blog/BlogPage";
+import AuthorPage from "./components/author/AuthorPage";
 
 function App() {
   return (
@@ -8,8 +10,8 @@ function App() {
       <Layout>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/blogs/:slug" element={} />
-          <Route path="/" element={<HomePage />} />
+          <Route path="/blogs/:slug" element={<BlogPage />} />
+          <Route path="/authors/:slug" element={<AuthorPage />} />
         </Routes>
       </Layout>
     </>
