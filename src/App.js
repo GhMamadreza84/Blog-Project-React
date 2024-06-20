ef146e71f22bf0e3bc5fdb482dc916eada45b342
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-06-20 12:42:21 -0700

    add new route blogs

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import HomePage from "./components/home/HomePage";
 import Layout from "./components/layout";
 import BlogPage from "./components/blog/BlogPage";
 import AuthorPage from "./components/author/AuthorPage";
+import Blogs from "./components/blog/Blogs";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/blogs/:slug" element={<BlogPage />} />
+          <Route path="/blogs" element={<Blogs />} />
           <Route path="/authors/:slug" element={<AuthorPage />} />
         </Routes>
       </Layout>
