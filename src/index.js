6ca6ce27c438dae4749dd1c0770fa502f04f1ff9
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-06-20 09:56:00 -0700

    wrap browser router in index.js

--- src/index.js
+++ src/index.js
@@ -4,21 +4,23 @@ import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import { ThemeProvider } from "@emotion/react";
 import theme from "./mui/theme";
 
-
 import App from "./App";
 import "./styles/index.css";
 import "./styles/fonts.css";
+import { BrowserRouter } from "react-router-dom";
 
 const client = new ApolloClient({
-  uri: process.env.REACT_APP_GRAPHCMS_URI, 
+  uri: process.env.REACT_APP_GRAPHCMS_URI,
   cache: new InMemoryCache(),
 });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ApolloProvider client={client}>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </ApolloProvider>
 );
