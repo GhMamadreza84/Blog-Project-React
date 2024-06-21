ba0573f38c2e2c6b66163fa1277f59d8a4ab3a50
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-06-21 03:20:11 -0700

    get loading and data and errors from GET_AUTHOR_INFO with useQuery Hook

--- src/components/author/AuthorPage.js
+++ src/components/author/AuthorPage.js
@@ -1,9 +1,11 @@
+import { useQuery } from "@apollo/client";
 import React from "react";
 import { useParams } from "react-router-dom";
+import { GET_AUTHOR_INFO } from "../../graphql/queries";
 
 function AuthorPage() {
-  const params = useParams();
-  console.log(params);
+  const { slug } = useParams();
+  const { loading, data, errors } = useQuery(GET_AUTHOR_INFO(slug));
   return <div>Author page</div>;
 }
 
