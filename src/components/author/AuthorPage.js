56a2992c7bf0d76496a75da8842b38a92ac5d390
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-06-21 03:45:38 -0700

    add avatar to author page

--- src/components/author/AuthorPage.js
+++ src/components/author/AuthorPage.js
@@ -3,7 +3,7 @@ import { useQuery } from "@apollo/client";
 import { useParams } from "react-router-dom";
 
 import { GET_AUTHOR_INFO } from "../../graphql/queries";
-import { Container } from "@mui/material";
+import { Avatar, Container, Grid } from "@mui/material";
 
 function AuthorPage() {
   const { slug } = useParams();
@@ -16,11 +16,15 @@ function AuthorPage() {
   if (loading) return <h3>Loading ...</h3>;
   if (errors) return <h3>Errors ...</h3>;
   console.log(data);
-  return 
-  <Container maxWidth="lg" >
-
-  </Container>
-  ;
+  return (
+    <Container maxWidth="lg">
+      <Grid container>
+        <Grid item>
+          <Avatar src={data.author.avatar.url} />
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
 
 export default AuthorPage;
