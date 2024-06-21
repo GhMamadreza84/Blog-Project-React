b178bf2dfdcbfb929cf54b81517fecc81503e0bc
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-06-21 12:22:24 -0700

    complate comment form html

--- src/components/comment/CommentForm.js
+++ src/components/comment/CommentForm.js
@@ -1,7 +1,10 @@
-import React from "react";
-import { Grid, Typography } from "@mui/material";
+import React, { useState } from "react";
+import { Button, Grid, TextField, Typography } from "@mui/material";
 
 function CommentForm({ slug }) {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [text, setText] = useState("");
   return (
     <Grid
       container
@@ -16,6 +19,44 @@ function CommentForm({ slug }) {
         <Typography component="p" variant="h6" fontWeight={700} color="primary">
           فرم ارسال کامنت
         </Typography>
+      </Grid>
+      <Grid item xs={12} m={2}>
+        <TextField
+          label="نام کاربری"
+          variant="outlined"
+          sx={{ width: "100%" }}
+          value={name}
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
+      </Grid>
+      <Grid item xs={12} m={2}>
+        <TextField
+          label="ایمیل "
+          variant="outlined"
+          sx={{ width: "100%" }}
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+      </Grid>
+      <Grid item xs={12} m={2}>
+        <TextField
+          label="متن کامنت "
+          variant="outlined"
+          sx={{ width: "100%" }}
+          value={text}
+          onChange={(e) => {
+            setText(e.target.value);
+          }}
+          multiline
+          minRows={4}
+        />
+      </Grid>
+      <Grid item xs={12} m={2}>
+        <Button variant="contained">ارسال</Button>
       </Grid>
     </Grid>
   );
