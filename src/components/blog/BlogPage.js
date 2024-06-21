581921dac8d77ee8cafaf90d5324508a0ff742d3
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-06-21 06:12:35 -0700

    get data from queries component with useQuery

--- src/components/blog/BlogPage.js
+++ src/components/blog/BlogPage.js
@@ -1,9 +1,16 @@
+import { useQuery } from "@apollo/client";
 import React from "react";
 import { useParams } from "react-router-dom";
+import { GET_BLOG_INFO } from "../../graphql/queries";
 
 function BlogPage() {
-  const params = useParams();
-  console.log(params)
+  const { slug } = useParams();
+  const { loading, data, errors } = useQuery(GET_BLOG_INFO, {
+    variables: {
+      slug,
+    },
+  });
+  console.log(params);
   return <div>BlogPage</div>;
 }
 
