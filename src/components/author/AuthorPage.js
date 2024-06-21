0d49eec8b6902509acf131e8f28a1ce5197d695b
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-06-21 03:21:40 -0700

    add slug to GET_AUTHOR_INFO

--- src/components/author/AuthorPage.js
+++ src/components/author/AuthorPage.js
@@ -1,11 +1,16 @@
-import { useQuery } from "@apollo/client";
 import React from "react";
+import { useQuery } from "@apollo/client";
 import { useParams } from "react-router-dom";
+
 import { GET_AUTHOR_INFO } from "../../graphql/queries";
 
 function AuthorPage() {
   const { slug } = useParams();
-  const { loading, data, errors } = useQuery(GET_AUTHOR_INFO(slug));
+  const { loading, data, errors } = useQuery(GET_AUTHOR_INFO(), {
+    variables: {
+      slug,
+    },
+  });
   return <div>Author page</div>;
 }
 
