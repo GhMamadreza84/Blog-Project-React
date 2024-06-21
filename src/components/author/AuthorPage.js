2272e4eb6771308d8be07590cbd759ce07f75be2
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-06-21 03:23:14 -0700

    edit authorpage

--- src/components/author/AuthorPage.js
+++ src/components/author/AuthorPage.js
@@ -6,12 +6,12 @@ import { GET_AUTHOR_INFO } from "../../graphql/queries";
 
 function AuthorPage() {
   const { slug } = useParams();
-  const { loading, data, errors } = useQuery(GET_AUTHOR_INFO(), {
+  const { loading, data, errors } = useQuery(GET_AUTHOR_INFO, {
     variables: {
       slug,
     },
   });
-  
+
   if (loading) return <h3>Loading ...</h3>;
   if (errors) return <h3>Errors ...</h3>;
   console.log(data);
