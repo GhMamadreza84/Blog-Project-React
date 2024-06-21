f66886d3890872f743c2b837485e2e51f81063f7
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-06-21 06:49:42 -0700

    i use hook usenavigate for when click on arrow icon go back before page

--- src/components/blog/BlogPage.js
+++ src/components/blog/BlogPage.js
@@ -1,25 +1,31 @@
 import { useQuery } from "@apollo/client";
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { GET_BLOG_INFO } from "../../graphql/queries";
 import { Container, Grid, Typography } from "@mui/material";
-
+import ArrowBackRoundedIcon from "@mui/icons-material/ArrowBackRounded";
 function BlogPage() {
   const { slug } = useParams();
+  const navigate = useNavigate();
   const { loading, data, errors } = useQuery(GET_BLOG_INFO, {
     variables: {
       slug,
     },
   });
   console.log(data);
-  // console.log(params);
-  return(
+  return (
     <Container maxWidth="lg">
       <Grid contianer>
-        <Grid item xs="12" mt={9}>
-          <Typography component="h2" variant="h4" color="primary" fontWeight={700}>
+        <Grid item xs="12" mt={9} display="flex" justifyContent="space-between">
+          <Typography
+            component="h2"
+            variant="h4"
+            color="primary"
+            fontWeight={700}
+          >
             {data.post.title}
           </Typography>
+          <ArrowBackRoundedIcon onClick={() => navigate(-1)} />
         </Grid>
       </Grid>
     </Container>
