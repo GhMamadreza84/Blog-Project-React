0b02df71fd444284842d31df29b57822a2e11e74
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-06-22 01:30:03 -0700

    add grid container and grid item in comments component

--- src/components/comment/Comments.js
+++ src/components/comment/Comments.js
@@ -1,15 +1,32 @@
 import { useQuery } from "@apollo/client";
 import React from "react";
 import { GET_POST_COMMENT } from "../../graphql/queries";
+import { Grid, Typography } from "@mui/material";
 
-function Comments({slug}) {
+function Comments({ slug }) {
   const { loading, data, errors } = useQuery(GET_POST_COMMENT, {
     variables: {
       slug,
     },
   });
-  console.log({ loading, data  });
-  return <div></div>;
+  if (loading) return null;
+  return (
+    <Grid
+      container
+      sx={{
+        boxShadow: "rgba(0,0,0,0.1) 0px 4px 12px",
+        borderRadius: 4,
+        py: 1,
+        mt: 8,
+      }}
+    >
+      <Grid item xs={12} m={2}>
+        <Typography component="p" variant="h6" fontWeight={700} color="primary">
+          کامنت ها
+        </Typography>
+      </Grid>
+    </Grid>
+  );
 }
 
 export default Comments;
