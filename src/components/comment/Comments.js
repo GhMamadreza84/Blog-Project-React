078e56e26004506e66cab3513d30e512e8ba974f
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-06-21 23:33:57 -0700

    get data with useQuery in comment component

--- src/components/comment/Comments.js
+++ src/components/comment/Comments.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import { useQuery } from "@apollo/client";
+import React from "react";
+import { GET_POST_COMMENT } from "../../graphql/queries";
 
-function Comments() {
-  return (
-    <div>
-      
-    </div>
-  )
+function Comments({slug}) {
+  const { loading, data, errors } = useQuery(GET_POST_COMMENT, {
+    variables: {
+      slug,
+    },
+  });
+  console.log({ loading, data  });
+  return <div></div>;
 }
 
-export default Comments
+export default Comments;
